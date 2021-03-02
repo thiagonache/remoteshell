cecef97352f95ccbf4b30f68b45307eb9d8effec
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2021-03-02 17:15:20 -0300

    Remove test for unexistent function

--- remoteshell_test.go
+++ remoteshell_test.go
@@ -53,17 +53,3 @@ func TestProtocolMismatch(t *testing.T) {
 		t.Errorf("want %q, got %q", want, got)
 	}
 }
-
-func TestProtocolActionAuth(t *testing.T) {
-	commands := []string{"hello", "auth abc1234"}
-	for _, cmd := range commands {
-		want := "OK\n"
-		got, err := remoteshell.ProtocolAction(cmd)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if want != got {
-			t.Errorf("want %q, got %q", want, got)
-		}
-	}
-}
